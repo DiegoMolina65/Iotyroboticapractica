8e5a0b7b11c79247815f97a2da9d6df2c715c4ab
DiegoMolina65 <diegobrian042@gmail.com>
2023-05-31 17:32:26 -0400

    modified:   topicoled/topicoled.html 	modified:   topicoled/topicoled.py

--- topicoled/topicoled.py
+++ topicoled/topicoled.py
@@ -1,29 +1,15 @@
-import paho.mqtt.client as mqtt
+from flask import Flask, request
 
-# Configuración del cliente MQTT
-broker_host = "104.154.170.98"
-broker_port = 1883
-client = mqtt.Client()
+app = Flask(__name__)
 
-# Configuración de las credenciales de autenticación (si es necesario)
-client.username_pw_set(username="diegomolina", password="diegomolina")
+@app.route('/led', methods=['POST'])
+def control_led():
+    data = request.get_json()
+    estado = data['estado']
+    
+    # Aquí puedes implementar la lógica para controlar el LED según el estado recibido
+    
+    return 'OK'
 
-# Función para manejar la conexión establecida con el broker MQTT
-def on_connect(client, userdata, flags, rc):
-    print("Conectado al broker MQTT")
-    # Suscribirse al tópico del LED después de la conexión establecida
-    client.subscribe("led_topic")
-
-# Función para manejar los mensajes recibidos desde el tópico
-def on_message(client, userdata, msg):
-    print("Mensaje recibido: " + msg.payload.decode())
-
-# Configuración de los manejadores de eventos
-client.on_connect = on_connect
-client.on_message = on_message
-
-# Conexión al broker MQTT
-client.connect(broker_host, broker_port, 60)
-
-# Bucle principal para mantener la conexión y procesar los mensajes
-client.loop_forever()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
