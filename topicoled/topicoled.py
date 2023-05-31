b203abf195cebae3c3fd1d6ae7446fd90f652e53
DiegoMolina65 <diegobrian042@gmail.com>
2023-05-31 17:43:18 -0400

    modified:   topicoled/topicoled.html 	modified:   topicoled/topicoled.py

--- topicoled/topicoled.py
+++ topicoled/topicoled.py
@@ -2,19 +2,30 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+# Estado inicial del LED
+led_encendido = False
+
 # Ruta para encender el LED
 @app.route('/encender_led', methods=['POST'])
 def encender_led():
-    # Lógica para encender el LED aquí
-    print("Encender LED")
-    return 'LED encendido'
+    global led_encendido
+    if not led_encendido:
+        led_encendido = True
+        print("LED encendido")
+        return 'LED encendido'
+    else:
+        return 'LED ya está encendido'
 
 # Ruta para apagar el LED
 @app.route('/apagar_led', methods=['POST'])
 def apagar_led():
-    # Lógica para apagar el LED aquí
-    print("Apagar LED")
-    return 'LED apagado'
+    global led_encendido
+    if led_encendido:
+        led_encendido = False
+        print("LED apagado")
+        return 'LED apagado'
+    else:
+        return 'LED ya está apagado'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
