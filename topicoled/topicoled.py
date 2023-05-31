3e20a71819ba824dd78ab60e382509c4575406cb
DiegoMolina65 <diegobrian042@gmail.com>
2023-05-31 17:41:18 -0400

    modified:   topicoled/topicoled.html 	modified:   topicoled/topicoled.py

--- topicoled/topicoled.py
+++ topicoled/topicoled.py
@@ -2,14 +2,19 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/led', methods=['POST'])
-def control_led():
-    data = request.get_json()
-    estado = data['estado']
-    
-    # Aquí puedes implementar la lógica para controlar el LED según el estado recibido
-    
-    return 'OK'
+# Ruta para encender el LED
+@app.route('/encender_led', methods=['POST'])
+def encender_led():
+    # Lógica para encender el LED aquí
+    print("Encender LED")
+    return 'LED encendido'
+
+# Ruta para apagar el LED
+@app.route('/apagar_led', methods=['POST'])
+def apagar_led():
+    # Lógica para apagar el LED aquí
+    print("Apagar LED")
+    return 'LED apagado'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
