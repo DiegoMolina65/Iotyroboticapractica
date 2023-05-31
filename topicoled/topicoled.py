5a31d87f89f443897146cef28dd3814c9da0a630
DiegoMolina65 <diegobrian042@gmail.com>
2023-05-31 16:56:09 -0400

    new file:   topicoled/topicoled.html 	new file:   topicoled/topicoled.py

---
+++ topicoled/topicoled.py
@@ -0,0 +1,46 @@
+import paho.mqtt.client as mqtt
+import time
+
+# Configuración del cliente MQTT
+broker_host = "104.154.170.98"
+broker_port = 1883
+client = mqtt.Client()
+
+# Configuración de las credenciales de autenticación (si es necesario)
+client.username_pw_set(username="diegomolina", password="diegomolina")
+
+# Función para manejar la conexión establecida con el broker MQTT
+def on_connect(client, userdata, flags, rc):
+    print("Conectado al broker MQTT")
+    # Suscribirse al tópico del LED después de la conexión establecida
+    client.subscribe("led_topic")
+
+# Función para manejar los mensajes recibidos desde el tópico
+def on_message(client, userdata, msg):
+    print("Mensaje recibido: " + msg.payload.decode())
+
+# Configuración de los manejadores de eventos
+client.on_connect = on_connect
+client.on_message = on_message
+
+# Conexión al broker MQTT
+client.connect(broker_host, broker_port, 60)
+
+# Bucle principal para mantener la conexión y procesar los mensajes
+client.loop_start()
+
+# Publicar un mensaje en el tópico del LED
+client.publish("led_topic", "Encender LED")
+
+# Esperar 5 segundos antes de publicar otro mensaje
+time.sleep(5)
+
+# Publicar otro mensaje en el tópico del LED
+client.publish("led_topic", "Apagar LED")
+
+# Esperar 5 segundos antes de finalizar el programa
+time.sleep(5)
+
+# Detener el bucle principal y desconectarse del broker MQTT
+client.loop_stop()
+client.disconnect()
